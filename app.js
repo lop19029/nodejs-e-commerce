f86195dcb6f3cfd8bf3285c29d232cd22384a683
lop19029 <alexhumbertolds@gmail.com>
2021-10-10 01:06:40 -0300

    Set up to deploy in Heroku

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors')
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -32,7 +33,23 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoose.connect('mongodb+srv://iClient:QJvwxay3Wo3hNSog@cluster0.8buii.mongodb.net/shop?retryWrites=true&w=majority')
+const corsOptions = {
+    origin: "https://<your_app_name>.herokuapp.com/",
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+const options = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    family: 4
+};
+
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://<username>:<username>@cse341cluster-3dwlw.mongodb.net/test?retryWrites=true&w=majority";
+
+mongoose.connect(MONGODB_URL, options)
 .then(result => {
     User.findOne().then(user => {
         if(!user){
